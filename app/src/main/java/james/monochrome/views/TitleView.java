4479153993ae4d2c485fb24f2ca05185e30dc1ae
James Fenn <18jafenn90@gmail.com>
2017-08-14 12:22:00 -0400

    added a neat title animation

--- app/src/main/java/james/monochrome/views/TitleView.java
+++ app/src/main/java/james/monochrome/views/TitleView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
@@ -15,11 +16,22 @@ public class TitleView extends AppCompatImageView {
             new int[]{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0},
             new int[]{0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0},
             new int[]{0},
-            new int[]{1},
+            new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             new int[]{0},
     };
 
+    private int[][] drawnTitle = new int[][]{
+            new int[1],
+            new int[43],
+            new int[43],
+            new int[43],
+            new int[1],
+            new int[43],
+            new int[1]
+    };
+
     private Paint paint;
+    private Handler handler;
 
     public TitleView(Context context) {
         super(context);
@@ -40,19 +52,51 @@ public class TitleView extends AppCompatImageView {
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.BLACK);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isFinished()) {
+                    invalidate();
+                    handler.postDelayed(this, 50);
+                }
+            }
+        }, 250);
+    }
+
+    private boolean isFinished() {
+        for (int i = 0; i < IMAGE_TITLE.length; i++) {
+            for (int i2 = 0; i2 < IMAGE_TITLE[i].length; i2++) {
+                if (drawnTitle[i][i2] != IMAGE_TITLE[i][i2])
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        int pixelHeight = canvas.getHeight() / IMAGE_TITLE.length;
-        for (int i = 0; i < IMAGE_TITLE.length; i++) {
-            int[] row = IMAGE_TITLE[i];
+        int copied = 0;
+        while (copied == 0) {
+            int copyY = (int) (Math.random() * IMAGE_TITLE.length);
+            int copyX = (int) (Math.random() * IMAGE_TITLE[copyY].length);
+            if (drawnTitle[copyY][copyX] != 1) {
+                copied = IMAGE_TITLE[copyY][copyX];
+                drawnTitle[copyY][copyX] = copied;
+            }
+        }
+
+        int pixelHeight = canvas.getHeight() / drawnTitle.length;
+        for (int i = 0; i < drawnTitle.length; i++) {
+            int[] row = drawnTitle[i];
             int pixelWidth = canvas.getWidth() / row.length;
             for (int i2 = 0; i2 < row.length; i2++) {
                 int x = i2 * pixelWidth, y = i * pixelHeight;
-                if (IMAGE_TITLE[i][i2] > 0)
+                if (drawnTitle[i][i2] > 0)
                     canvas.drawRect(x, y, x + pixelWidth, y + pixelHeight, paint);
             }
         }
